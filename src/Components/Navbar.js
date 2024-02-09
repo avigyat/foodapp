b015fd21bcd429c2b6a77a46eebd36ec6bbf67db
avigyat <avigyat.avi@outlook.com>
2024-02-09 15:14:19 +0530

    login logout functioality completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     let location = useLocation();
+    const navigate = useNavigate();
 
     React.useEffect(() => {
-      console.log(location.pathname);
+        console.log(location.pathname);
     }, [location]);
+
+    const logout = () =>{
+        
+        localStorage.removeItem('token')
+        navigate('/login')
+    }
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -16,12 +23,17 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <form className='d-flex topnav-right' >
+                    {(localStorage.getItem('token')===null||localStorage.getItem('token')==='null'||localStorage.getItem('token')==='undefined'||localStorage.getItem('token')===undefined)
+        ?<form className='d-flex topnav-right' >
+        
+          <Link  className='btn mx-2 btn-primary' to='/login'>Login</Link>
+          <Link  className='btn mx-2 btn-primary' to='/signUp'>Sign Up</Link>
+        </form>
+        :<form className='d-flex topnav-right' >      
+        <button  className='btn mx-2 btn-primary' onClick={logout}>Cart</button>
+        <button  className='btn mx-2 btn-danger' onClick={logout}>Log out</button>
+        </form>}
 
-                        <Link className='btn mx-2 btn-primary' to='/login'>Login</Link>
-                        <Link className='btn mx-2 btn-primary' to='/signUp'>Sign Up</Link>
-                    </form>
-  
                 </div>
 
             </nav>
